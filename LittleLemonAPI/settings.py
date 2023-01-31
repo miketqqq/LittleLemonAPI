067c62a20b033e23a6c761a7639031c51ad1c756
Mike <miketsang12@gmail.com>
2023-01-31 19:37:18 +0800

    first commit

--- LittleLemonAPI/settings.py
+++ LittleLemonAPI/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a7(7vd4n9444r31xeaj-!&+z$%%5$$d=4a3k_ffg)iid8nzza='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=False) 
 
 ALLOWED_HOSTS = []
 
